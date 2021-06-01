61f49dbcffa541fda5ef290dd77345e6385af99b
unknown <jimpidas>
2021-06-01 23:06:48 +0530

    [Jyosmita]Added:Premium user for the ride and calculating total fare

--- CanInvoiceGeneratorTest/UnitTest1.cs
+++ CanInvoiceGeneratorTest/UnitTest1.cs
@@ -44,5 +44,15 @@ namespace CanInvoiceGeneratorTest
             InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0, "1241");
             Assert.AreEqual(expectedSummary, summary);
         }
+        [Test]
+        public void GivenRides_ForPremiumUser_ShouldReturnTotalFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            double distance = 3.0;
+            int time = 10;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 65;
+            Assert.AreEqual(expected, fare);
+        }
     }
 }
